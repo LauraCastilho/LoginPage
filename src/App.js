8ee75968e106f145bf7271e7246b7ebb46dfb685
Laura De Andrade Castilho <lcastilho@tecnologia-unica.com.br>
2022-04-12 01:45:25 -0300

    LoginPage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './style.css';
 
 function App() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form">
+
+            <span className="login-form-tittle">Welcome</span>
+
+            <div className="wrap-input">
+              <input className={email !== "" ? 'has-val login-form-input' : 'login-form-input'} type="email" value={email} onChange = {e => setEmail(e.target.value)}></input>
+              <span className="focus-input" data-placeholder="Email"></span>
+            </div>
+
+            <div className="wrap-input">
+              <input className={password !== "" ? 'has-val login-form-input' : 'login-form-input'} type="password" value={password} onChange = {e => setPassword(e.target.value)}></input>
+              <span className="focus-input" data-placeholder="Password"></span>
+            </div>
+
+            <div className="container-login-form-btn">
+              <button className="login-form-btn">Login</button>
+            </div>
+
+            <div className="container-login-create-account">
+              <span className="txt1">Don't have an account yet?</span>
+              <a className="txt2" href="#">Sign Up</a>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
